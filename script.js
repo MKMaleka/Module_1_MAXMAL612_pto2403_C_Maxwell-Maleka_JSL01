e6ebeb30e9a2f5c36206a1b1aa231d862197110e
maxwell <maxwell.maleka@sintrex.com>
2024-07-12 10:10:23 +0200

    [mm] Improved the function to validate using regex

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    let regex = /^pet_(\d{4})+([A-Za-z]+)$/; //The string startswith pet_,captures four digits of brith year and only contain letters 
+    let result = input.match(regex); 
+    
+    if (result) {
+        let birthYear = result[1]; //extracts birth year from the matched regex
+        let name = result[2]; //extracts name from the matched regex      
+        
+        let BirthYear = birthYear >= 1500 && birthYear <= new Date().getFullYear();  //validated between 1500 and current year
+        let Name = /^[A-Za-z]+$/.test(name) && name.trim().length > 0; //match a string that contains uppercase and lowercase, test(name) to check string match regex
+        
 
-            document.getElementById('result').innerText = result;
+        let display = BirthYear && Name ? "Valid Syntax🟢" : "Invalid Syntax🔴"; //used boolean to determine if criteria is met or not
+        document.getElementById('result').innerText = display;
+      } 
+    else {
+        document.getElementById('result').innerText = "Invalid Syntax🔴";
+    }
+
 }
 
 
